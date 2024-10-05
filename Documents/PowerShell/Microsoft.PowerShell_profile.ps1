4a789d87f281f2eff7519645e467bd91c2ea289f
Léo Colombaro <git@colombaro.fr>
2024-10-05 15:32:49 +0200

    Tweak PSReadLine options

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,32 +13,44 @@ Invoke-Expression (&starship init powershell)
 
 # Load special features come from PSReadLine
 if (Get-Module PSReadLine) {
-    Set-PSReadlineOption -HistoryNoDuplicates
+    # Searching for commands with up/down arrow is really handy.  The
+    # option "moves to end" is useful if you want the cursor at the end
+    # of the line while cycling through history like it does w/o searching,
+    # without that option, the cursor will remain at the position it was
+    # when you used up arrow, which can be useful if you forget the exact
+    # string you started the search on.
+    Set-PSReadLineOption -HistoryNoDuplicates
     Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-    Set-PSReadlineKeyHandler -Chord UpArrow -Function HistorySearchBackward
-    Set-PSReadlineKeyHandler -Chord DownArrow -Function HistorySearchForward
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+    
     # Sometimes you enter a command but realize you forgot to do something else first.
     # This binding will let you save that command in the history so you can recall it,
     # but it doesn't actually execute.  It also clears the line with RevertLine so the
     # undo stack is reset - though redo will still reconstruct the command line.
-    Set-PSReadlineKeyHandler -Chord Alt+s `
+    Set-PSReadLineKeyHandler -Key Alt+w `
                              -BriefDescription SaveInHistory `
                              -LongDescription "Save current line in history but do not execute" `
                              -ScriptBlock {
         param($key, $arg)
+    
         $line = $null
         $cursor = $null
         [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
         [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
     }
+
     # This key handler shows the entire or filtered history using Out-GridView. The
     # typed text is used as the substring pattern for filtering. A selected command
     # is inserted to the command line without invoking. Multiple command selection
     # is supported, e.g. selected by Ctrl + Click.
-    Set-PSReadlineKeyHandler -Chord F7 `
+    # As another example, the module 'F7History' does something similar but uses the
+    # console GUI instead of Out-GridView. Details about this module can be found at
+    # PowerShell Gallery: https://www.powershellgallery.com/packages/F7History.
+    Set-PSReadLineKeyHandler -Key F7 `
                              -BriefDescription History `
-                             -LongDescription "Show command history" `
+                             -LongDescription 'Show command history' `
                              -ScriptBlock {
         $pattern = $null
         [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
@@ -46,10 +58,11 @@ if (Get-Module PSReadLine) {
         {
             $pattern = [regex]::Escape($pattern)
         }
+    
         $history = [System.Collections.ArrayList]@(
             $last = ''
             $lines = ''
-            foreach ($line in [System.IO.File]::ReadLines((Get-PSReadlineOption).HistorySavePath))
+            foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath))
             {
                 if ($line.EndsWith('`'))
                 {
@@ -64,11 +77,13 @@ if (Get-Module PSReadLine) {
                     }
                     continue
                 }
+    
                 if ($lines)
                 {
                     $line = "$lines`n$line"
                     $lines = ''
                 }
+    
                 if (($line -cne $last) -and (!$pattern -or ($line -match $pattern)))
                 {
                     $last = $line
@@ -77,6 +92,7 @@ if (Get-Module PSReadLine) {
             }
         )
         $history.Reverse()
+    
         $command = $history | Out-GridView -Title History -PassThru
         if ($command)
         {
@@ -84,23 +100,27 @@ if (Get-Module PSReadLine) {
             [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
         }
     }
+
     # F1 for help on the command line - naturally
-    Set-PSReadlineKeyHandler -Chord F1 `
+    Set-PSReadLineKeyHandler -Key F1 `
                              -BriefDescription CommandHelp `
                              -LongDescription "Open the help window for the current command" `
                              -ScriptBlock {
         param($key, $arg)
+    
         $ast = $null
         $tokens = $null
         $errors = $null
         $cursor = $null
         [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+    
         $commandAst = $ast.FindAll( {
             $node = $args[0]
             $node -is [System.Management.Automation.Language.CommandAst] -and
                 $node.Extent.StartOffset -le $cursor -and
                 $node.Extent.EndOffset -ge $cursor
             }, $true) | Select-Object -Last 1
+    
         if ($commandAst -ne $null)
         {
             $commandName = $commandAst.GetCommandName()
@@ -111,12 +131,12 @@ if (Get-Module PSReadLine) {
                 {
                     $commandName = $command.ResolvedCommandName
                 }
+    
                 if ($commandName -ne $null)
                 {
-                    Get-Help -Name $commandName
+                    Get-Help $commandName -ShowWindow
                 }
             }
         }
     }
 }
-
